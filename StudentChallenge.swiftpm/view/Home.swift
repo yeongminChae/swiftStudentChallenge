8929c582f8e347c40abc1df2d3c67e53251b6538
Yeongmin_Chae <codudals98@naver.com>
2023-04-19 11:33:04 +0900

    Modify the Home file UI

--- StudentChallenge.swiftpm/view/Home.swift
+++ StudentChallenge.swiftpm/view/Home.swift
@@ -27,18 +27,7 @@ struct Home: View {
                     HStack {
                         Text("SNO").frame(maxWidth: .infinity, alignment: .center).font(.system(size: 25,weight: .semibold)).padding(EdgeInsets(top: 15, leading: 0, bottom: 10, trailing: 0))
                     }
-                        
                     ZStack{
-                        
-//                        Rectangle()
-//                            .fill(Color(.white))
-//                            .overlay(
-//                            Text("Choose What You Want").background(Color(.white)).font(.system(size: 20,weight: .semibold)).padding(.top,-180).padding(.leading,20))
-//                            .onTapGesture {
-//                                isClicked1 = false
-//                                isClicked2 = false
-//                            }
-                        
                         NavigationLink(destination: cardViewModel.cardData[0].view().navigationBarBackButtonHidden(false)) {
                             RoundedRectangle(cornerRadius: 20)
                                 .fill(Color.white)
@@ -55,64 +44,52 @@ struct Home: View {
                                     }
                                 )
                         }
-                        
-//                        LazyHStack {
-//                            ForEach(0..<3, id: \.self)  { i in
-//
-//                            }.animation(Animation.spring())
-//                        }
-//                        .modifier(ScrollingHStackModifier(items: 3 , itemWidth: 250, itemSpacing: 30)).padding(.top,-40)
-//                            .offset(x:geometry.size.width/(-10) , y:geometry.size.height/(500))
+                    
                     }.onTapGesture {
                         isClicked1 = false
                         isClicked2 = false
                     }
                     
-                    ZStack{
+//                    ZStack{
                         
-                        // 버튼 밖으로 빼기.
+                    VStack{
+//                        Rectangle()
+//                            .overlay(Image("people2").resizable().frame(height:300))
+//                            .padding(.bottom,20)
+//                        if settings.stampArr.count == 6 {
+                        NavigationLink(destination: EmotionDefine().navigationBarBackButtonHidden(true)) {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                                .frame(width: 210, height: 170)
+                                .shadow(radius: 10)
+                                .padding()
+                                .overlay(
+                                    ZStack {
+                                        Image("people2")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("CUSTOM")
+                                            .padding(.top,130)
+                                    }
+                                )
+                        }
                         
-                        Rectangle()
-                            .overlay(Image("people2").resizable().frame(height:300)).frame(width: UIScreen.main.bounds.width, height: 300)
-                            .padding(.bottom,20)
-                        
-                        if settings.stampArr.count == 6 {
-//                            NavigationLink(destination: EmotionDefineCheck().navigationBarBackButtonHidden(true),
-//                            isActive: $shouldNavigateToCheck) {
+//                            NavigationLink(destination: EmotionDefine().navigationBarBackButtonHidden(true),
+//                                           isActive: $shouldNavigateToDefine) {
 //                                EmptyView()
 //                            }
-//
 //                            Button(action: {
-//                                shouldNavigateToCheck = true
-//                                shouldNavigateToDefine = false
+//                                shouldNavigateToDefine = true
 //                            }, label: {
 //                                RoundedRectangle(cornerRadius: 20).frame(width:100,height: 60).shadow(radius: 10)
-//                                    .overlay(Text("TO CHECK").foregroundColor(Color(.white)))
+//                                    .overlay(Text("CUSTOM").foregroundColor(Color(.white)))
 //                            }
-//                            ).offset(x:geometry.size.width/(3.5) , y:geometry.size.height/(9))
-                            NavigationLink(destination: EmotionDefine().navigationBarBackButtonHidden(true),
-                                isActive: $shouldNavigateToDefine) {
-                                EmptyView()
-                            }
-                            
-                            Button(action: {
-                                shouldNavigateToCheck = false
-                                shouldNavigateToDefine = true
-                            }, label: {
-                                RoundedRectangle(cornerRadius: 20).frame(width:100,height: 60).shadow(radius: 10)
-                                    .overlay(Text("CUSTOM").foregroundColor(Color(.white)))
-                            }
-                            ).offset(x:geometry.size.width/(3.5) , y:geometry.size.height/(9))
-                        }
-//                        else {
-//
+//                            )
 //                        }
-                        
-                    }
-//                    .padding(.leading,160)
+                    }.frame(width: UIScreen.main.bounds.width, height: 300)
+//                    }
                     
                 }
-//                .offset(x:geometry.size.width/(-1.526) , y:geometry.size.height/(500))
             }.onTapGesture {
                 isClicked1 = false
                 isClicked2 = false
